442f07abd8067977399b94aac23b6cf70329e495
jancy <jancyj@jancys-MBP.attlocal.net>
2024-06-01 00:42:46 -0500

    added ranomd username generator

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,13 +1,67 @@
-import { StyleSheet } from 'react-native';
+import { StyleSheet } from "react-native";
 
-import EditScreenInfo from '@/components/EditScreenInfo';
-import { Text, View } from '@/components/Themed';
+import EditScreenInfo from "@/components/EditScreenInfo";
+import { Text, View } from "@/components/Themed";
+import { generateUsername } from "unique-username-generator";
+import React, { useEffect, useState } from "react";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import database from '../../node_modules/@react-native-firebase/database';
+// import firebaseConfig from '../../firebaseConfig';
+
+
+const reference = database().ref('post');
+
+console.log("firebase: ", reference);
+
+
 
 export default function TabOneScreen() {
+
+  const createUserName = () => {
+    console.log("Creating a user");
+    fetch("https://randomuser.me/api/?format=json")
+      .then((res) => res.json())
+      .then((data) => data.results.map((i: any) => storeData(i.id.value)));
+  };
+
+  const getData = async () => {
+    try {
+      const value = await AsyncStorage.getItem("username");
+      if (value === null) {
+        console.log("No user created yet");
+        createUserName();
+      } else {
+        console.log("User: ", value)
+      }
+    } catch (e) {
+      console.log("error: ", e);
+    }
+  };
+
+  const storeData = async (value: string) => {
+    console.log("Storing username locally");
+    try {
+      await AsyncStorage.setItem("username", value);
+    } catch (e) {
+      console.log("Error saving locally");
+      return e;
+    }
+  };
+
+  useEffect(() => {
+    getData();
+  }, []);
+
+
+
   return (
     <View style={styles.container}>
       <Text style={styles.title}>Tab One</Text>
-      <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
+      <View
+        style={styles.separator}
+        lightColor="#eee"
+        darkColor="rgba(255,255,255,0.1)"
+      />
       <EditScreenInfo path="app/(tabs)/index.tsx" />
     </View>
   );
@@ -16,16 +70,16 @@ export default function TabOneScreen() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
+    alignItems: "center",
+    justifyContent: "center",
   },
   title: {
     fontSize: 20,
-    fontWeight: 'bold',
+    fontWeight: "bold",
   },
   separator: {
     marginVertical: 30,
     height: 1,
-    width: '80%',
+    width: "80%",
   },
 });
---
+++ firebase/firebaseConfig.tsx
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyDz16Oyk-unBegCE9FISaKXf45j3UKuqb0",
+    authDomain: "pickup-basketball-7d716.appspot.com",
+    projectId: "pickup-basketball-7d716",
+    storageBucket: "pickup-basketball-7d716.appspot.com",
+    messagingSenderId: "586029158060",
+    appId: "1:586029158060:ios:b919a11e5a5a8d7162d909",// Optional, only if you're using Firebase Analytics
+  };
+  
+  export default firebaseConfig;
+  
